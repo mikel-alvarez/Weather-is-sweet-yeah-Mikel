59a2d8e9e8442442d14b7de1abfe5f73f39e1084
Mikel alvarez <mikelalvarez271@gmail.com>
2021-10-21 12:17:54 +0200

    v2: implementacion de localizacion

--- www/js/menu.js
+++ www/js/menu.js
@@ -2,10 +2,18 @@ $(function () {
   $("#btnhome").on("click", function () {
     $("#home").show();
     $("#seach").hide();
+    $("#location").hide();
   });
   $("#btnseach").on("click", function () {
     $("#home").hide();
     $("#seach").show();
+    $("#location").hide();
   });
   $("#btnhome").click();
+  $("#btnlocation").on("click", function () {
+    $("#home").hide();
+    $("#seach").hide();
+    $("#location").show();
+    geoLocationGet();
+  });
 });
